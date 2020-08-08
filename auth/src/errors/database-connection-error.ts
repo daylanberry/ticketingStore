ea175b5a5286691c6ad9e24ff76a3cbe3472544b
Daylan <daylanberry@gmail.com>
2020-08-07 17:43:54 -0700

    created skaffold, creating error handlers

---
+++ auth/src/errors/database-connection-error.ts
@@ -0,0 +1,11 @@
+export class DatabaseConnectionError extends Error {
+  reason = 'Error connectiong to database';
+
+  constructor() {
+    super();
+
+
+    Object.setPrototypeOf(this, DatabaseConnectionError.prototype)
+  }
+
+}
---
+++ auth/src/routes/current-user.ts
@@ -0,0 +1,10 @@
+import express from 'express'
+
+const router = express.Router()
+
+router.get('/api/users/currentUser', (req, res) => {
+  res.send('Hi there')
+
+});
+
+export { router as currentUserRouter };
