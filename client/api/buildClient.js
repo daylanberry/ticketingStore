51c2bcc4545d5f98641af4a07d86502774e623d0
Daylan <daylanberry@gmail.com>
2020-08-13 11:44:35 -0700

    created sign up and signout logic on client

---
+++ client/api/buildClient.js
@@ -0,0 +1,18 @@
+import axios from 'axios';
+
+export default ({ req }) => {
+  if (typeof Window === 'undefined') {
+    // we're on the server
+    return axios.create({
+      baseURL: 'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local',
+      headers: req.headers
+    });
+
+  } else {
+    // we're on the browser
+    return axios.create({
+      baseURL: '/'
+    })
+
+  }
+}
--- client/pages/index.js
+++ client/pages/index.js
@@ -1,3 +1,20 @@
-export default () => {
-  return <h1>Landing Page!</h1>
+import buildClient from '../api/buildClient'
+
+const LandingPage = ({ currentUser }) => {
+
+  return currentUser ? (
+    <h1>You are signed in</h1>
+  ) : (
+    <h1>You are signed out</h1>
+  )
+};
+
+LandingPage.getInitialProps = async (context) => {
+  const client = buildClient(context)
+
+  const { data } = await client.get('/api/users/currentUser')
+
+  return data
 }
+
+export default LandingPage;
