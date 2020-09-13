c20f8efff2245ab3bf9380b204eb4cf4e200a1b4
Daylan <daylanberry@gmail.com>
2020-09-13 15:18:45 -0700

    created order listener

---
+++ tickets/src/events/listeners/order-created-listener.ts
@@ -0,0 +1,25 @@
+import { Listener, OrderCreatedEvent, Subjects } from '@dbtickets/common';
+import { queueGroupName } from './queue-group-name';
+import { Message } from 'node-nats-streaming';
+import { Ticket } from '../../models/ticket';
+
+class OrderCreatedListener extends Listener <OrderCreatedEvent>{
+  readonly subject = Subjects.OrderCreated
+  queueGroupName = queueGroupName;
+
+  async onMessage(data: OrderCreatedEvent['data'], msg: Message) {
+
+    const ticket = await Ticket.findById(data.ticket.id);
+
+    if (!ticket) {
+      throw new Error('Ticket not found')
+    }
+
+    ticket.set({ orderId: data.id })
+
+    await ticket.save();
+
+    msg.ack();
+
+  }
+}
