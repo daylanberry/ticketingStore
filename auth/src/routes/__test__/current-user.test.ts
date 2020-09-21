cacd88ff8016f53283927547785cf566449ca843
Daylan <daylanberry@gmail.com>
2020-09-21 15:17:21 -0700

    last minute change

--- auth/src/routes/__test__/current-user.test.ts
+++ auth/src/routes/__test__/current-user.test.ts
@@ -11,7 +11,7 @@ it('responds with details about the current user', async() => {
     .get('/api/users/currentUser')
     .set('Cookie', cookie)
     .send()
-    .expect(200)
+    .expect(400)
 
   expect(response.body.currentUser.email).toEqual('d@gmail.com')
 });
